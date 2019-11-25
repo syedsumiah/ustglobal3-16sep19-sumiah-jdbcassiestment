6c987cffc915d201f49a6d2655bfbac8946a3291
syedsumiah <sumiya.syed96@gmail.com>
2019-11-25 14:05:13 +0530

    jdbc-assessment

---
+++ assessment/src/com/ustglobal/assessment/dao/InformationDAO.java
@@ -0,0 +1,17 @@
+
+
+package com.ustglobal.assessment.dao;
+
+import java.util.List;
+
+import com.ustglobal.assessment.dto.InformationBean;
+
+public interface InformationDAO {
+
+	public List<String> getAllContactNames();
+	public InformationBean searchForContact(String name);
+
+
+}
+
+
